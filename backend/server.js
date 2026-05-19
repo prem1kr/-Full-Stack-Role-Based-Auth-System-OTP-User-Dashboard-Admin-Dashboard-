7a91686df817d72cefdf0b6fda952220e3611dc0
Prem1kr <prem78334@gmail.com>
2026-05-19 15:58:59 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import authRouter from './routes/authRoute.js';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
 
 app.use('/api/auth', authRouter);
 
