e978ad204db17ce9341eb8e29c6bde3fa280b4d8
Prem1kr <prem78334@gmail.com>
2026-05-21 14:40:51 +0530

    correction

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -64,7 +64,7 @@ export const Login = async (req, res) => {
 
         const token = jwt.sign({ id: user._id, email: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: '7d' });
         res.cookie('token', token, { httpOnly: true, secure: true, sameSite: "none", maxAge: 7 * 24 * 60 * 60 * 1000 });
-
+        console.log(req.cookies);
         const saferData = {
             id: user._id,
             email: user.email,
--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -1,14 +1,46 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const adminAuth = async (req, res, next) => {
     try {
+
+        console.log("cookies:", req.cookies);
+
         const token = req.cookies.token;
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (decoded.role !== "admin") {
-            return res.status(403).json({ success: false, message: "Admin access only" });
+
+        console.log("token:", token);
+
+        if (!token) {
+            return res.status(401).json({
+                success: false,
+                message: "No token found"
+            });
         }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        console.log("decoded:", decoded);
+
+        if (decoded.role !== "admin") {
+            return res.status(403).json({
+                success: false,
+                message: "Admin access only"
+            });
+        }
+
         req.user = decoded;
+
         next();
 
     } catch (error) {
-        return res.status(401).json({ success: false, message: "Unauthorized" });
+
+        console.log(error);
+
+        return res.status(401).json({
+            success: false,
+            message: "Unauthorized"
+        });
     }
 }
