e9849f4fe581df21fcd6ab6ecbb2edf30feb44b8
prem kumar <151729765+prem1kr@users.noreply.github.com>
2026-05-22 11:39:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3000' || 'https://smtp-role-based-and-otp-verification-ianu.onrender.com',
     credentials: true
 }));
 
